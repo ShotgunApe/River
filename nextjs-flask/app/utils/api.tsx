1e5ecd0a314612d20215a4f727a4510457cb5dcd
Will <shotgunayyype@gmail.com>
2025-02-22 16:48:19 -0500

    test

--- nextjs-flask/app/utils/api.tsx
+++ nextjs-flask/app/utils/api.tsx
@@ -1,20 +1,21 @@
-export const getReq = async function (lat: string | number, lon: string | number) {
+export const getReq = async function (lat: string, lon: string) {
     if (lat && lon) {
         var latTwo = lat.toString().match(/^-?\d+(?:\.\d{0,4})?/)![0]
         var lonTwo = lon.toString().match(/^-?\d+(?:\.\d{0,4})?/)![0]
         var json = await fetch("/api/get-weather?lat=" + latTwo + "&lon=" + lonTwo)
-        .then(response => {
-            response.json();
+        .then(response => response.json)
+        .then(data => {
+            console.log(data)
         })
-        return json;
+        return JSON.stringify(json);
     }
 };
 
-export const getPredict = (prediction: string) => {
+export const getPredict = async function (prediction: any) {
 
     //let obj = JSON.parse(JSON.stringify(prediction));
     const u = new URLSearchParams(prediction).toString();
-    fetch("/api/predict?data=" + prediction)
+    fetch("/api/predict?data=" + prediction[0])
     .then(response => response.json())  
     .then(json => {
         console.log(json);
