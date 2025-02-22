182133be3824d6997945558b88d8f723b4cde919
Will <shotgunayyype@gmail.com>
2025-02-21 23:10:19 -0500

    get request test with weather api

--- nextjs-flask/api/index.py
+++ nextjs-flask/api/index.py
@@ -1,6 +1,19 @@
+import requests
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/api/python")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/api/handle_get", methods=['GET'])
+def process():
+    testing = {
+        'value1': '1',
+        'value2': 'abcd'
+    }
+    return testing
+
+@app.route("/api/get_weather", methods=['GET'])
+def weather():
+    return requests.get('https://api.weather.gov/points/39.7456,-97.0892').content
