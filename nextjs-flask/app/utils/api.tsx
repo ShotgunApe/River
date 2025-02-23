c8d5059ea459427a9a50f871bf504002c76fd9e4
ShotgunApe <shotgunayyype@gmail.com>
2025-02-22 21:45:49 -0500

    working API functionalitygit status

--- nextjs-flask/app/utils/api.tsx
+++ nextjs-flask/app/utils/api.tsx
@@ -35,22 +35,59 @@ export const getReq = async function (lat: string | number, lon: string | number
             .then(response => response.json())  
             .then(data => {
                 let rain = data["properties"]["periods"][0]["probabilityOfPrecipitation"]["value"]
+                let avgSpeed = data["properties"]["periods"][0]["windSpeed"].slice(0,2)
+                let year = data["properties"]["periods"][0]["startTime"].slice(0,4)
+                let month = data["properties"]["periods"][0]["startTime"].slice(5,7)
+                let day = data["properties"]["periods"][0]["startTime"].slice(8,10)
+
+                let spring = 0
+                let summer = 0
+                let fall = 0
+                let winter = 0
+
+                let minTemp = 0
+                let maxTemp = 0
+
+                if (month >= 3 && month <= 5) {
+                    spring = 1
+                } else if (month >= 6 && month <= 8) {
+                    summer = 1
+                } else if (month >= 9 && month <= 11) {
+                    fall = 1
+                } else if (month === 12 || month <= 2) {
+                    winter = 1
+                }
+
+                // Check if data collection from weather api starts at night or day
+                if (data["properties"]["periods"][0]["startTime"].slice(11,13) == 18) {
+                    minTemp = data["properties"]["periods"][0]["temperature"];
+                    maxTemp = data["properties"]["periods"][1]["temperature"];
+                } else {
+                    minTemp = data["properties"]["periods"][1]["temperature"];
+                    maxTemp = data["properties"]["periods"][0]["temperature"];
+                }
+
+                // this should NEVER be negative, but I would rather play it safe than sorry
+                let tempRange = Math.abs(maxTemp - minTemp)
+
+                let winTempRatio = (avgSpeed / maxTemp);
+
                 let toSend: trenches = {
-                    "PRECIPITATION": rain,
-                    "MAX_TEMP": 0,
-                    "MIN_TEMP": 0,
-                    "AVG_WIND_SPEED": 0,
-                    "YEAR": 0,
-                    "TEMP_RANGE": 0,
-                    "WIND_TEMP_RATIO": 0,
-                    "MONTH": 0,
-                    "LAGGED_PRECIPITATION": 0,
-                    "LAGGED_AVG_WIND_SPEED": 0,
-                    "DAY_OF_YEAR": 0,
-                    "Fall": 0,
-                    "Spring": 0,
-                    "Summer": 0,
-                    "Winter": 0
+                    "PRECIPITATION": (rain/100),
+                    "MAX_TEMP": maxTemp,
+                    "MIN_TEMP": minTemp,
+                    "AVG_WIND_SPEED": avgSpeed,
+                    "YEAR": year,
+                    "TEMP_RANGE": tempRange,
+                    "WIND_TEMP_RATIO": winTempRatio,
+                    "MONTH": month,
+                    "LAGGED_PRECIPITATION": 0.5,
+                    "LAGGED_AVG_WIND_SPEED": 6,
+                    "DAY_OF_YEAR": day,
+                    "Fall": fall,
+                    "Spring": spring,
+                    "Summer": summer,
+                    "Winter": winter
                 };
                 
                 postReq(toSend)
